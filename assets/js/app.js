6f06046190f98e81ca2a86423a3aaa150d4f9795
Diego Moura Torres <95168133+dieggo-torres@users.noreply.github.com>
2022-04-05 08:54:47 -0300

    Remova algumas classes

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@
 const anoAtual = new Date().getFullYear();
 
 // Obtém o parágrafo que contém o ano de copyright
-const paragrafoRodape = document.querySelector('.copyright');
+const paragrafoRodape = document.querySelector('#footer p.lead');
 
 // Conteúdo do parágrafo de copyright
 paragrafoRodape.textContent = `© Copyright ${anoAtual}, Tindog.`
